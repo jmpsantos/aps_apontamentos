aed43f9959487593e34724f93b23a01505f03fa1
jmpsantos <jmpsantos148@gmail.com>
2022-11-07 23:27:08 +0000

    Adapter + Bridge

---
+++ aps_padroes_estruturais/aps_adapter.java
@@ -0,0 +1,91 @@
+/*Adapter: Padrão que permite a colaboração entre objetos de diferentes interfaces 
+ * 
+ * Funcionamento: 1. O Adapter comunica com uma das interfaces comaptiveis com os objetos
+ *                2. Através desta interface o objeto existente chama os metodos do adaptador
+ *                3. Após ser chamado o adaptador passa o request ao segundo objeto no formato que este espera
+ * 
+ * Utilizar quando: - Temos uma classe/serviço que não é compatível com o código
+*/
+
+/**
+ * RoundHoles are compatible with RoundPegs.
+ */
+public class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg) {
+        boolean result;
+        result = (this.getRadius() >= peg.getRadius());
+        return result;
+    }
+}
+
+/**
+ * RoundPegs are compatible with RoundHoles.
+ */
+public class RoundPeg {
+    private double radius;
+
+    public RoundPeg() {}
+
+    public RoundPeg(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
+
+/**
+ * SquarePegs are not compatible with RoundHoles (they were implemented by
+ * previous development team). But we have to integrate them into our program.
+ */
+public class SquarePeg {
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getSquare() {
+        double result;
+        result = Math.pow(this.width, 2);
+        return result;
+    }
+}
+
+
+/**
+ * Adapter allows fitting square pegs into round holes.
+ */
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
+
+
+ /*https://refactoring.guru/design-patterns/adapter*/
